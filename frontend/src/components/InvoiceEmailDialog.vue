38d5afa333455518a0b974ccfbc72a1196d38ebd
David Pierron <dpierron@gmail.com>
2026-05-02 16:18:33 +0200

    fix(invoices): use <object type=application/pdf> for cross-browser PDF preview (TEC-146)

--- frontend/src/components/InvoiceEmailDialog.vue
+++ frontend/src/components/InvoiceEmailDialog.vue
@@ -65,11 +65,12 @@
       <div class="invoice-email-dialog__preview">
         <p class="invoice-email-dialog__preview-label">{{ t('invoices.email_preview') }}</p>
         <Skeleton v-if="pdfLoading" class="invoice-email-dialog__embed" border-radius="4px" />
-        <iframe
+        <object
           v-else-if="pdfBlobUrl"
-          :src="pdfBlobUrl"
+          :data="pdfBlobUrl"
+          type="application/pdf"
           class="invoice-email-dialog__embed"
-          :title="t('invoices.email_preview_title')"
+          :aria-label="t('invoices.email_preview_title')"
         />
         <div v-else class="invoice-email-dialog__preview-empty">
           <i class="pi pi-file-pdf" />
